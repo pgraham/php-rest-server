8b244700f4c5c14d8a43b0dd97612e387a43d991
Philip Graham <philip.robert.graham@gmail.com>
2012-03-15 17:09:59 +0000

    Fixed base namespace

--- zeptech/rest/ResponseEncoder.php
+++ zeptech/rest/ResponseEncoder.php
@@ -12,7 +12,7 @@
  *
  * @license http://www.opensource.org/licenses/bsd-license.php
  */
-namespace conductor\rest;
+namespace zeptech\rest;
 
 /**
  * Interface for objects which encode a response for a specific media type and
