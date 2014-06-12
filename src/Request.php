9c9c888c97d64b06ada7cda07fa85a80fd9aef71
Philip Graham <philip.robert.graham@gmail.com>
2014-06-12 17:06:16 +0000

    [minor] CS fixes

--- src/Request.php
+++ src/Request.php
@@ -1,20 +1,15 @@
 <?php
-/**
- * =============================================================================
- * Copyright (c) 2010, Philip Graham
+/*
+ * Copyright (c) 2014, Philip Graham
  * All rights reserved.
  *
- * This file is part of php-rest-server and is licensed by the Copyright holder
- * under the 3-clause BSD License.  The full text of the license can be found in
- * the LICENSE.txt file included in the root directory of this distribution or
- * at the link below.
- * =============================================================================
- *
- * @license http://www.opensource.org/licenses/bsd-license.php
+ * This file is part of php-rest-server. For the full copyright and license
+ * information please view the LICENSE file that was distributed with this
+ * source code.
  */
 namespace zpt\rest;
 
-use \StdClass;
+use StdClass;
 
 /**
  * This class encapsulates data about a resource request.
@@ -23,170 +18,171 @@ use \StdClass;
  */
 class Request {
 
-  /* The HTTP method of the request. */
-  private $action;
+	/* The HTTP method of the request. */
+	private $action;
 
-  /* Data sent with the request. */
-  private $_data;
+	/* Data sent with the request. */
+	private $data;
 
-  /* The id of the mapping that handles this request. */
-  private $_mappingId;
+	/* The id of the mapping that handles this request. */
+	// This is a code smell, this is the responsibility of the Router or RouteHandler
+	private $mappingId;
 
-  /* Values for any parameters specified in the mapping's URI template */
-  private $_parameters;
+	/* Values for any parameters specified in the mapping's URI template */
+	private $parameters;
 
-  /* Query parameters sent with the request. */
-  private $_query;
+	/* Query parameters sent with the request. */
+	private $query;
 
-  /* The requested URI */
-  private $_uri;
+	/* The requested URI */
+	private $url;
 
-  public function __construct($uri, $mappingId = null) {
-    $this->_uri = $uri;
-    $this->_mappingId = $mappingId;
-  }
+	public function __construct($url, $mappingId = null) {
+		$this->url = $url;
+		$this->mappingId = $mappingId;
+	}
 
-  /**
-   * Getter for the request's HTTP method.
-   *
-   * @return string
-   */
-  public function getAction() {
-    return $this->action;
-  }
+	/**
+	 * Getter for the request's HTTP method.
+	 *
+	 * @return string
+	 */
+	public function getAction() {
+		return $this->action;
+	}
 
-  /**
-   * Getter for the request data.
-   *
-   * @return array
-   */
-  public function getData($key = null) {
-    if ($key !== null) {
-      if ($this->_data instanceof StdClass) {
-        return $this->_data->$key;
-      } else if (is_array($this->_data)) {
-        return $this->_data[$key];
-      } else {
-        return null;
-      }
-    }
-    return $this->_data;
-  }
+	/**
+	 * Getter for the request data.
+	 *
+	 * @return array
+	 */
+	public function getData($key = null) {
+		if ($key !== null) {
+			if ($this->data instanceof StdClass) {
+				return $this->data->$key;
+			} else if (is_array($this->data)) {
+				return $this->data[$key];
+			} else {
+				return null;
+			}
+		}
+		return $this->data;
+	}
 
-  /**
-   * Getter for the mapping that is handling the request.
-   *
-   * @return string
-   */
-  public function getMappingId() {
-    return $this->_mappingId;
-  }
+	/**
+	 * Getter for the mapping that is handling the request.
+	 *
+	 * @return string
+	 */
+	public function getMappingId() {
+		return $this->mappingId;
+	}
 
-  /**
-   * Getter for the parameter with the given name.
-   *
-   * @return mixed
-   */
-  public function getParameter($parameter) {
-    if (isset($this->_parameters[$parameter])) {
-      return $this->_parameters[$parameter];
-    }
-    return null;
-  }
+	/**
+	 * Getter for the parameter with the given name.
+	 *
+	 * @return mixed
+	 */
+	public function getParameter($parameter) {
+		if (isset($this->parameters[$parameter])) {
+			return $this->parameters[$parameter];
+		}
+		return null;
+	}
 
-  /**
-   * Getter for the expanded values of the request's matching URI template.
-   *
-   * @return array
-   */
-  public function getParameters() {
-    return $this->_parameters;
-  }
+	/**
+	 * Getter for the expanded values of the request's matching URI template.
+	 *
+	 * @return array
+	 */
+	public function getParameters() {
+		return $this->parameters;
+	}
 
-  /**
-   * Getter for the request's query parameters.
-   *
-   * @return array
-   */
-  public function getQuery() {
-    return $this->_query;
-  }
+	/**
+	 * Getter for the request's query parameters.
+	 *
+	 * @return array
+	 */
+	public function getQuery() {
+		return $this->query;
+	}
 
-  /**
-   * Getter for the request's query parameter with the given name.
-   *
-   * @param string $name The name of the query parameter to fetch.
-   * @return string
-   */
-  public function getQueryParameter($key) {
-    if (isset($this->_query[$key])) {
-      return $this->_query[$key];
-    }
-    return null;
-  }
+	/**
+	 * Getter for the request's query parameter with the given name.
+	 *
+	 * @param string $name The name of the query parameter to fetch.
+	 * @return string
+	 */
+	public function getQueryParameter($key) {
+		if (isset($this->query[$key])) {
+			return $this->query[$key];
+		}
+		return null;
+	}
 
-  /**
-   * Getter for the request's URI.
-   *
-   * @return string
-   */
-  public function getUri() {
-    return $this->_uri;
-  }
+	/**
+	 * Getter for the request's URI.
+	 *
+	 * @return string
+	 */
+	public function getUri() {
+		return $this->url;
+	}
 
-  /**
-   * Indicate whether or not the specified data value is present in the
-   * request.
-   *
-   * @param string $key
-   * @return boolean
-   */
-  public function hasData($key) {
-    if ($this->_data instanceof StdClass) {
-      return isset($this->_data->$key);
-    } else if (is_array($this->_data)) {
-      return isset($this->_data[$key]);
-    } else {
-      return false;
-    }
-  }
+	/**
+	 * Indicate whether or not the specified data value is present in the
+	 * request.
+	 *
+	 * @param string $key
+	 * @return boolean
+	 */
+	public function hasData($key) {
+		if ($this->data instanceof StdClass) {
+			return isset($this->data->$key);
+		} else if (is_array($this->data)) {
+			return isset($this->data[$key]);
+		} else {
+			return false;
+		}
+	}
 
-  /**
-   * Setter for the request's HTTP method.
-   *
-   * @param string $action
-   */
-  public function setAction($action) {
-    $this->action = $action;
-  }
+	/**
+	 * Setter for the request's HTTP method.
+	 *
+	 * @param string $action
+	 */
+	public function setAction($action) {
+		$this->action = $action;
+	}
 
-  /**
-   * Setter for any raw data sent with the request.  This is typically the
-   * request body, preprocessed by PHP.
-   *
-   * @param array $data
-   */
-  public function setData($data) {
-    $this->_data = $data;
-  }
+	/**
+	 * Setter for any raw data sent with the request.  This is typically the
+	 * request body, preprocessed by PHP.
+	 *
+	 * @param array $data
+	 */
+	public function setData($data) {
+		$this->data = $data;
+	}
 
-  /**
-   * Setter for any expanded values for the URI template that matches this
-   * request.
-   *
-   * @param array $parameters
-   */
-  public function setParameters(array $parameters = null) {
-    $this->_parameters = $parameters;
-  }
+	/**
+	 * Setter for any expanded values for the URI template that matches this
+	 * request.
+	 *
+	 * @param array $parameters
+	 */
+	public function setParameters(array $parameters = null) {
+		$this->parameters = $parameters;
+	}
 
-  /**
-   * Setter for any query parameters sent with the request.
-   *
-   * @param array $query
-   */
-  public function setQuery(array $query = null) {
-    $this->_query = $query;
-  }
+	/**
+	 * Setter for any query parameters sent with the request.
+	 *
+	 * @param array $query
+	 */
+	public function setQuery(array $query = null) {
+		$this->query = $query;
+	}
 
 }
