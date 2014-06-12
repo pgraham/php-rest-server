7c6bf0946000b9aee466829c7d5ff57331c21790
Philip Graham <philip.robert.graham@gmail.com>
2014-06-12 03:14:25 +0000

    [minor] CS fixes: use tabs for indentation

--- src/UriTemplate.php
+++ src/UriTemplate.php
@@ -16,48 +16,48 @@ namespace zpt\rest;
  */
 class UriTemplate {
 
-  private $_tmpl;
-  private $_matchRE;
-  private $_paramNames = array();
+	private $_tmpl;
+	private $_matchRE;
+	private $_paramNames = array();
 
-  public function __construct($uriTemplate) {
-    $this->_tmpl = $uriTemplate;
+	public function __construct($uriTemplate) {
+		$this->_tmpl = $uriTemplate;
 
-    if ($this->_tmpl === '/') {
-      $this->_matchRE = '/^\/$/';
-      return;
-    }
+		if ($this->_tmpl === '/') {
+			$this->_matchRE = '/^\/$/';
+			return;
+		}
 
-    if(preg_match_all('/\/\{([^}]+)\}/', $this->_tmpl, $matches)) {
-      $this->_paramNames = $matches[1];
-    }
+		if(preg_match_all('/\/\{([^}]+)\}/', $this->_tmpl, $matches)) {
+			$this->_paramNames = $matches[1];
+		}
 
-    $tmplCmps = explode('/', ltrim($this->_tmpl, '/'));
-    $escaped = array();
-    foreach ($tmplCmps as $cmp) {
-      if (preg_match('/\{.+\}/', $cmp)) {
-        $escaped[] = '([^\/]+)';
-      } else {
-        $escaped[] = preg_quote($cmp);
-      }
-    }
+		$tmplCmps = explode('/', ltrim($this->_tmpl, '/'));
+		$escaped = array();
+		foreach ($tmplCmps as $cmp) {
+			if (preg_match('/\{.+\}/', $cmp)) {
+				$escaped[] = '([^\/]+)';
+			} else {
+				$escaped[] = preg_quote($cmp);
+			}
+		}
 
-    $this->_matchRE = "/^\/" . implode('\/', $escaped) . "$/";
-  }
+		$this->_matchRE = "/^\/" . implode('\/', $escaped) . "$/";
+	}
 
-  public function matches($uri, &$params = false) {
-    if (!$params) {
-      $params = [];
-    }
-    if (preg_match($this->_matchRE, $uri, $matches)) {
-      array_shift($matches);
+	public function matches($uri, &$params = false) {
+		if (!$params) {
+			$params = [];
+		}
+		if (preg_match($this->_matchRE, $uri, $matches)) {
+			array_shift($matches);
 
-      $params = array();
-      foreach ($this->_paramNames AS $idx => $param) {
-        $params[$param] = $matches[$idx];
-      }
-      return true;
-    }
-    return false;
-  }
+			$params = array();
+			foreach ($this->_paramNames AS $idx => $param) {
+				$params[$param] = $matches[$idx];
+			}
+			return true;
+		}
+		return false;
+	}
 }
